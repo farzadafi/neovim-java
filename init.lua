f4344dbe1e91fc96dbbf8f24e9828daa9acd43fe
farzadafi <farzadafi40@gmail.com>
2024-10-09 21:31:19 +0330

    add some config for render lazy git on open nvim

--- init.lua
+++ init.lua
@@ -16,7 +16,26 @@ end
 -- Add the path to the lazy plugin repositories to the vim runtime path
 vim.opt.rtp:prepend(lazypath)
 
+-- Declare a few options for lazy
+local opts = {
+	change_detection = {
+		-- Don't notify us every time a change is made to the configuration
+		notify = false,
+	},
+	checker = {
+		-- Automatically check for package updates
+		enabled = true,
+		-- Don't spam us with notification every time there is an update available
+		notify = false,
+	},
+}
 
 -- Load the options from the config/options.lua file
 require("config.options")
 require("config.keymaps")
+
+-- Setup lazy, this should always be last
+-- Tell lazy that all plugin specs are found in the plugins directory
+-- Pass it the options we specified above
+require("lazy").setup("plugins", opts)
+
