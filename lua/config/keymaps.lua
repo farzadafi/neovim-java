6246ea854c48b42fc4e3fb15decff3df7f6fd4ca
farzadafi <farzadafi40@gmail.com>
2024-10-09 21:06:46 +0330

    fix some comma and > in some line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,14 +16,14 @@ vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>" "<C-w><C-h", { desc = "Move foucus to the left window" })
-vim.keymap.set("n", "<C-l>" "<C-w><C-l", { desc = "Move foucus to the right window" })
-vim.keymap.set("n", "<C-j>" "<C-w><C-j", { desc = "Move foucus to the lower window" })
-vim.keymap.set("n", "<C-k>" "<C-w><C-k", { desc = "Move foucus to the upper window" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move foucus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move foucus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move foucus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move foucus to the upper window" })
 
 -- Easily split windows
-vim.keymap.set("n", "<leader>wv" ":vsplit<cr>", { desc = "[W]indows split [V]ertical" })
-vim.keymap.set("n", "<leader>wh" ":split<cr>", { desc = "[W]indows split [H]orizontal" })
+vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", { desc = "[W]indows split [V]ertical" })
+vim.keymap.set("n", "<leader>wh", ":split<cr>", { desc = "[W]indows split [H]orizontal" })
 
 -- Syay in indent mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
