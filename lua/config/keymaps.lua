7000ac4f85b741a9379053a242c8949c354161e3
farzadafi <farzadafi40@gmail.com>
2024-10-09 19:56:33 +0330

    add key map for remove highlights code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Anywhere you see <leader> in a keymapping specifies the space key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Remove search highlights after searching
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>, { desc = "Remove search highlights" })
