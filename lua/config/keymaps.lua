73c4b654f6307d408a3d8a849bc8edb2cc82d743
farzadafi <farzadafi40@gmail.com>
2024-10-09 20:07:33 +0330

    add key map for window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,9 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>" "<C-w><C-h", { desc = "Move foucus to the left window" })
+vim.keymap.set("n", "<C-l>" "<C-w><C-l", { desc = "Move foucus to the right window" })
+vim.keymap.set("n", "<C-j>" "<C-w><C-j", { desc = "Move foucus to the lower window" })
+vim.keymap.set("n", "<C-k>" "<C-w><C-k", { desc = "Move foucus to the upper window" })
