d3ad91de7184f7f0bde33401e3d4f3fc82c255c6
farzadafi <farzadafi40@gmail.com>
2024-10-09 18:56:45 +0330

    add left column and similar setting section

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Left column and similar settings
+vim.opt.number = true -- display line numbers
+vim.opt.relativenumber = true -- display relative line numbers
+vim.opt.numberwidth = 2 -- set width of line number column
+vim.opt.signcolumn = "yes" -- always show sign column
+vim.opt.wrap = false -- display lines as single line
+vim.opt.scrolloff = 10 -- number of lines to keep above/below cursor
+vim.opt.sidescrolloff = 8 -- number of columns to keep to the left/right of cursor
