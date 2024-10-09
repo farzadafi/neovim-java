9d811249288cdc461f721017dc94466c19fab16e
farzadafi <farzadafi40@gmail.com>
2024-10-09 20:05:40 +0330

    add new key map for exit terminal mode and disable arrow key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,12 @@ vim.g.maplocalleader = " "
 
 -- Remove search highlights after searching
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
+
+-- Exit Vim's terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Optional: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
