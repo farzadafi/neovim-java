0bd04b0414609500fe266781535de0392066e0bf
farzadafi <farzadafi40@gmail.com>
2024-10-10 10:18:53 +0330

    fix lazy git config :)

--- init.lua
+++ init.lua
@@ -3,38 +3,63 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Check to see if lazy itself has been cloned, if not clone it into the lazy.nvim directory
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath
-  })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 -- Add the path to the lazy plugin repositories to the vim runtime path
 vim.opt.rtp:prepend(lazypath)
- 
--- Declare a few options for lazy
-local opts = {
- 	change_detection = {
-	-- Don't notify us every time a change is made to the configuration
- 		notify = false,
- 	},
- 	checker = {
- 		-- Automatically check for package updates
- 		enabled = true,
- 		-- Don't spam us with notification every time there is an update available
- 		notify = false
- 	}
-}
 
 -- Load the options from the config/options.lua file
 require("config.options")
 require("config.keymaps")
 
--- Setup lazy, this should always be last
--- Tell lazy that all plugin specs are found in the plugins directory
--- Pass it the options we specified above
-require("lazy").setup("plugins", opts)
+---- Setup lazy, this should always be last
+---- Tell lazy that all plugin specs are found in the plugins directory
+---- Pass it the options we specified above
+require("lazy").setup({
+	spec = {
+		-- add LazyVim and import its plugins
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		-- import/override with your plugins
+		{ import = "plugins" },
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	install = { colorscheme = { "tokyonight", "habamax" } },
+	checker = {
+		enabled = true, -- check for plugin updates periodically
+		notify = false, -- notify on update
+	}, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
