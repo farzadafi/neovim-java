2a4ef5f9d7a4497ce33d51ea89b45a8d21b893d7
farzadafi <farzadafi40@gmail.com>
2024-11-21 21:52:56 +0330

    chagne again from cu to cw

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -76,7 +76,7 @@ return {
 			-- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
 			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
         -- Key mapping for Go to Definition
-      vim.keymap.set("n", "<leader>cu", "<Cmd>lua vim.lsp.buf.definition()<CR>", { desc = "[C]ode [U]se" })
+      vim.keymap.set("n", "<leader>cw", "<Cmd>lua vim.lsp.buf.definition()<CR>", { desc = "[C]ode [W]here [D]efinition" })
 		end,
 	},
 }
